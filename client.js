f06fbe6b41b07b1f4c29d1c5c66e8fd7125b47e4
Chandani Kumari <chandaanikumari92@gmail.com>
2023-08-02 21:16:31 -0700

    added initials

--- client.js
+++ client.js
@@ -5,13 +5,20 @@ const connect = function() {
     host: '165.227.47.243',// IP address here,
     port: '50541', // PORT number here,
   });
-
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    // sets up an event handler
+    console.log(`Successfully connected to game server`);
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("connect", () => {
   // code that does something when the connection is first established
     // sets up an event handler
     console.log(`you ded cuz you idled`);
+  });
+  conn.on("connect", () => {
+    conn.write("Name: SRI");
   });
   return conn;
 };
--- play.js
+++ play.js
@@ -1,6 +1,4 @@
-const net = require("net");
 const connect = require("./client");
-//const port = 50541;
 
 console.log("Connecting ...");
 connect();
